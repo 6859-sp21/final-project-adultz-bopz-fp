cb266c2486e845cc909a82922fa1500f867b27a0
Nico Salinas <salinas.nsalinas@gmail.com>
2021-04-29 12:55:55 -0500

    Reverting bubble data fetch to A4 work. Does not affect timeline

--- src/utils/data-transform.js
+++ src/utils/data-transform.js
@@ -9,47 +9,42 @@ export const genRawData = async () => {
 export const genNestedData = async () => {
   const csvData = await genRawData();
 
-  let groupedByCategory = Array.from(d3.group(csvData, (d) => d.category)).map(
-    (category) => {
-      let groupedByArtist = Array.from(
-        d3.group(category[1], (d) => d.ogArtist)
-      ).map((item) => {
-        let groupedByBadword = Array.from(
-          d3.group(item[1], (d) => d.badword)
-        ).map((word) => {
-          let groupedBySong = Array.from(
-            d3.group(word[1], (d) => d.songName)
-          ).map((song) => {
-            // Remove repeated lyrics
-            let groupedByLyric = Array.from(
-              d3.group(song[1], (d) => d.ogLyric)
-            ).map((uniqueLyricGroup) => {
-              // uniqueLyricGroup = is a size-2 array with lyric and index 0 and array of data points at index 1
-              let dataEntries = uniqueLyricGroup[1];
-              let firstUniqueEntry = dataEntries[0];
-              const { ogLyric, kbLyric, badword } = firstUniqueEntry;
+  let groupedByArtist = Array.from(
+      d3.group(csvData, (d) => d.ogArtist)
+    ).map((item) => {
+      let groupedByBadword = Array.from(
+        d3.group(item[1], (d) => d.badword)
+      ).map((word) => {
+        let groupedBySong = Array.from(
+          d3.group(word[1], (d) => d.songName)
+        ).map((song) => {
+          // Remove repeated lyrics
+          let groupedByLyric = Array.from(
+            d3.group(song[1], (d) => d.ogLyric)
+          ).map((uniqueLyricGroup) => {
+            // uniqueLyricGroup = is a size-2 array with lyric and index 0 and array of data points at index 1
+            let dataEntries = uniqueLyricGroup[1];
+            let firstUniqueEntry = dataEntries[0];
+            const { ogLyric, kbLyric, badword } = firstUniqueEntry;
 
-              let { kbLyricHTML, ogLyricHTML } = compareLyrics(
-                badword,
-                ogLyric,
-                kbLyric
-              );
-              return {
-                ...firstUniqueEntry,
-                kbLyricHTML: kbLyricHTML,
-                ogLyricHTML: ogLyricHTML,
-              };
-            });
-            return { name: song[0], children: groupedByLyric };
+            let { kbLyricHTML, ogLyricHTML } = compareLyrics(
+              badword,
+              ogLyric,
+              kbLyric
+            );
+            return {
+              ...firstUniqueEntry,
+              kbLyricHTML: kbLyricHTML,
+              ogLyricHTML: ogLyricHTML,
+            };
           });
-          return { name: word[0], children: groupedBySong };
+          return { name: song[0], children: groupedByLyric };
         });
-        return { name: item[0], children: groupedByBadword };
+        return { name: word[0], children: groupedBySong };
       });
-      return { name: category[0], children: groupedByArtist };
-    }
-  );
-  return groupedByCategory;
+      return { name: item[0], children: groupedByBadword };
+    });
+  return groupedByArtist;
 };
 
 export const genTimelineData = async () => {
