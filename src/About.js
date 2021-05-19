401a374e330650e0fe528631f2a0aa24c7862d9c
Nico Salinas <salinas.nsalinas@gmail.com>
2021-05-18 20:53:45 -0700

    updated project page with video demo

--- src/About.js
+++ src/About.js
@@ -19,6 +19,8 @@ const About = () => {
               <Link to="/app" className="launch-button">
                 Launch Visualization App
               </Link>
+
+              <p style={{marginTop: "24px"}}><i>Note: Use the left and right carousel arrows to navigate the project page. </i></p>
             </section>
             <section id="carousel2" className="About-section About-section-white-background">
               <h2>Abstract</h2>
@@ -26,27 +28,34 @@ const About = () => {
             </section>
 
             <section id="carousel3" className="About-section About-section-white-background">
-              <h2>VIDEO DEMO HERE</h2>
+              <h2>Project Trailer</h2>
+              <div class="video-container">
+                <iframe width="560" height="560" src="https://www.youtube.com/embed/39xePSyzGp0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+              <p>
+                Shannen Wu, Jessica Yin, and Nico Salinas  <br />
+                <i>(Guest performances from Matt Tung)</i>
+              </p>
             </section>
 
             <section id="carousel4" className="About-section About-section-white-background">
               <h2>INSTALLATION DEMO INSTRUCTIONS</h2>
               <div className="About-section-info">
-                <h4>Dataset URL: <a href="https://github.com/the-pudding/data/tree/master/kidz-bop">https://github.com/the-pudding/data/tree/master/kidz-bop</a></h4>
+                <h3>Dataset URL: <a className="About-section-info-link" target="_blank" href="https://github.com/the-pudding/data/tree/master/kidz-bop" rel="noreferrer">https://github.com/the-pudding/data/tree/master/kidz-bop</a></h3>
                 
-                <h4>Installation</h4>
+                <h3>Installation</h3>
                 <ol>
                   <li>Locally clone the repository</li>
                   <li>Run <code>npm install</code> to install all node dependices</li>
                   <li>To test locally, run <code>npm start</code> and open a Chrome tab at <code>http://localhost:3000</code></li>
                 </ol>
                 
-                <h4>Deployment</h4>
+                <h3>Deployment</h3>
                 The deployed site is managed by GitHub Pages. To deploy this repo:
                 <ol>
                   <li>Run <code>npm run predeploy</code></li>
                   <li>Run <code>npm run deploy</code></li>
-                  <li>Visit the deployed site at <a href="https://6859-sp21.github.io/final-project-adultz-bopz-fp/">https://6859-sp21.github.io/final-project-adultz-bopz-fp/</a></li>
+                  <li>Visit the deployed site at <a className="About-section-info-link" target="_blank" href="https://6859-sp21.github.io/final-project-adultz-bopz-fp/" rel="noreferrer">https://6859-sp21.github.io/final-project-adultz-bopz-fp/</a></li>
                 </ol>
               </div>
             </section>
